463a3a78d507777705e3851aa539e8874d47d259
Karii88 <88991745+Karii88@users.noreply.github.com>
2021-10-08 20:02:12 -0500

    Subiendo avance 6 de proyecto

--- proyecto_micro_pokedex.py
+++ proyecto_micro_pokedex.py
@@ -8,32 +8,20 @@ def consulta_pokemon(respuesta_usuario):
 
     if respuesta_usuario == pokemones[0]:
         print("-Estas son sus características base de combate:")
-        print(" PS:",ps[0])
-        print(" Ataque:",ataque[0])
-        print(" Defensa:",defensa[0])
-        print(" Ataque especial:",at_esp[0])
-        print(" Defensa especial:",def_esp[0])
-        print(" Velocidad:",velocidad[0])
+        datos_pokemon(pokemon1)
+        print()
         print(caracteristicas_pokemon(movpoke1,evolpoke1))
-                
+             
     elif respuesta_usuario == pokemones[1]:
         print("-Estas son sus características base de combate:")
-        print(" PS:",ps[1])
-        print(" Ataque:",ataque[1])
-        print(" Defensa:",defensa[1])
-        print(" Ataque especial:",at_esp[1])
-        print(" Defensa especial:",def_esp[1])
-        print(" Velocidad:",velocidad[1])
+        datos_pokemon(pokemon2)
+        print()
         print(caracteristicas_pokemon(movpoke2,evolpoke2))
                 
     elif respuesta_usuario == pokemones[2]:
         print("-Estas son sus características base de combate:")
-        print(" PS:",ps[2])
-        print(" Ataque:",ataque[2])
-        print(" Defensa:",defensa[2])
-        print(" Ataque especial:",at_esp[2])
-        print(" Defensa especial:",def_esp[2])
-        print(" Velocidad:",velocidad[2])
+        datos_pokemon(pokemon3)
+        print()
         print(caracteristicas_pokemon(movpoke3,evolpoke3))
 
 def caracteristicas_pokemon(movpoke,evolpoke):
@@ -57,23 +45,42 @@ def puntos_def_esp(def_esp1, def_esp2, def_esp3):
 def puntos_velocidad(velocidad1, velocidad2, velocidad3):
     return max(velocidad1, velocidad2, velocidad3)
 
-#-------------Variables------------------
+def datos_pokemon(matriz):
+    for linea in matriz:
+      for columna in linea:
+        print(columna, end = " ")
+      print()
+
+#-------------Variables, listas y matrices------------------
 poke1="Rillaboom"
 poke2="Cinderace"
 poke3="Inteleon"
 pokemones=["Rillaboom","Cinderace","Inteleon"]
-ps=["100","80","70"]
-ataque=["125","116","85"]
-defensa=["90","75","65"]
-at_esp=["60","65","125"]
-def_esp=["70","75","65"]
-velocidad=["85","119","120"]
 movpoke1 = "-Estos son los movimientos que puede aprender subiendo de nivel:\n Batería asalto\n Doble golpe\n Campo de hierba\n Rugido de guerra\n Arañazo\n Gruñido\n Punzada rama\n Mofa\n Hoja afilada\n Chirrido\n Desarme\n Atizar\n Alboroto\n Mazazo\n Esfuerzo\n Estruendo"
 movpoke2 = "-Estos son los movimientos que puede aprender subiendo de nivel:\n Balon ígneo\n Amago\n Placaje\n Gruñido\n Ascuas\n Ataque rápido\n Doble patada\n Nitrocarga\n Agilidad\n Golpe cabeza\n Contraataque\n Bote\n Doble filo\n Cambio de cancha"
 movpoke3 = "-Estos son los movimientos que puede aprender subiendo de nivel:\n Disparo certero\n Acróbata\n Destructor\n Gruñido\n Pistola agua\n Atadura\n Hidropulso\n Ojos llorosos\n Golpe bajo\n Ida y vuelta\n Hidroariete\n Anegar\n Danza lluvia\n Hidrobomba"
 evolpoke1 = "-Estas son sus pre-evoluciones:\n Grookey\n Thwackey"
 evolpoke2 = "-Estas son sus pre-evoluciones:\n Scorbunny\n Raboot"
 evolpoke3 = "-Estas son sus pre-evoluciones:\n Sobble\n Drizzile"
+
+pokemon1 = [[' Ps:','100'],
+         [' Ataque:','125'],
+         [' Defensa:', '90'],
+         [' Ataque especial:','60'],
+         [' Defensa especial:','70'],
+         [' Velocidad:','85']]
+pokemon2 = [[' Ps:','80'],
+         [' Ataque:','116'],
+         [' Defensa:', '75'],
+         [' Ataque especial:','65'],
+         [' Defensa especial:','75'],
+         [' Velocidad:','119']]
+pokemon3 = [[' Ps:','70'],
+         [' Ataque:','85'],
+         [' Defensa:', '65'],
+         [' Ataque especial:','125'],
+         [' Defensa especial:','65'],
+         [' Velocidad:','120']]
 #-------------Instrucciones para el usuario--------
 print("Estos son los Pokémon iniciales consultables de la región de Galar:", poke1, poke2, poke3)
 print("Teclea correctamente el nombre del Pokémon que deseas consultar:")
@@ -81,6 +88,7 @@ print("Teclea correctamente el nombre del Pokémon que deseas consultar:")
 respuesta_usuario=str(input())
 consulta_pokemon(respuesta_usuario)
 #--------------------------------------------------------------
+
 print("-Comparando puntos de vida, los más altos son: ", puntos_vida(100,80,70), "correspondientes a :", pokemones[0])
 
 print("-Comparando puntos de ataque, los más altos son: ", puntos_ataque(125,116,85), "correspondientes a :", pokemones[0])
@@ -92,6 +100,7 @@ print("-Comparando puntos de ataque especial, los más altos son: ", puntos_at_e
 print("-Comparando puntos de defensa especial, los más altos son: ", puntos_def_esp(70,75,65), "correspondientes a :", pokemones[1])
 
 print("-Comparando puntos de velocidad, los más altos son: ", puntos_velocidad(85,119,120), "correspondientes a :", pokemones[2])
+
 #---------------------------------------------------------------
 print("¿Deseas consultar otro pokémon? Teclea 'si' o 'no'")
 nueva_consulta=str(input())
@@ -107,7 +116,6 @@ while nueva_consulta == "si":
     consulta_pokemon(respuesta_usuario)
     print("¿Deseas consultar otro pokémon? Teclea 'si' o 'no'")
     nueva_consulta=str(input())
-        
 #EF
 print("¡Gracias por consultar en la micro pokédex!")
 
